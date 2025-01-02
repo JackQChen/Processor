bcb4e46f860cd6449dac87cdde634912b51f59aa
Jack Chen <jackqchen@outlook.com>
2025-01-02 23:22:19 +0800

    Update Program.cs

--- Processor/Program.cs
+++ Processor/Program.cs
@@ -12,10 +12,14 @@ namespace Processor
         static async Task Run()
         {
             using var playwright = await Playwright.CreateAsync();
-            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+                Args = new[] { "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36" }
+            });
             var page = await browser.NewPageAsync();
-            await page.GotoAsync("https://playwright.dev/dotnet");
-            await page.ScreenshotAsync(new() { Path = "screenshot.png" });
+            await page.GotoAsync("https://www.douyin.com/?recommend=1");
+            await page.PauseAsync();
         }
 
     }
