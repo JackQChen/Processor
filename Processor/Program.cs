f2792e60ce45c5f87ad03fffbb308502e8eb23d2
Jack Chen <jackqchen@outlook.com>
2025-01-02 13:17:29 +0800

    Init

---
+++ Processor/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Playwright;
+
+namespace Processor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Run().GetAwaiter().GetResult();
+        }
+
+        static async Task Run()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://playwright.dev/dotnet");
+            await page.ScreenshotAsync(new() { Path = "screenshot.png" });
+        }
+
+    }
+
+}
